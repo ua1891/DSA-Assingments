1dd078591de45c96944293927bded8a687c96ae7
Vendor Co <ua9118@gmail.com>
2026-04-28 11:47:30 +0500

    Make a Adja Matrix for Directed graph Task4A

---
+++ Graphs/Task4A.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <list>
+using namespace std;
+class Graph
+{
+    int V, E;
+    list<int> *Arr = nullptr;
+
+public:
+    Graph(int v, int e)
+    {
+        this->V = v;
+        this->E = e;
+        Arr = new list<int>[V];
+    }
+    void AddEdge(){
+        for (int i = 0; i < E; i++)
+        {
+            int Src,Des;
+            cin>>Src>>Des;
+            AddHelper(Src,Des);
+            cout<<"\n";
+        }
+        
+    }
+    void AddHelper(int Source, int destination)
+    {
+        Arr[Source].push_back(destination);//just Drected Graph
+    }
+    void Adja_matrix()
+    {
+        for (int i = 0; i < V; i++)
+        {
+                for (int j = 0; j < V; i++)
+                {
+                    
+                    
+                }
+                
+        
+            cout<<"\n";
+        }
+    }
+};
+
+int main()
+{
+    int Vertex, Edge;
+    cout << "Enter the Total Number of vertices ";
+    cin >> Vertex;
+    cout << "Enter the Total Number of Edges ";
+    cin >> Edge;
+    Graph object(Vertex,Edge);
+object.AddEdge();
+object.Adja_matrix();
+    return 0;
+}

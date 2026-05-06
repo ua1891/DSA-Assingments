d08a1612e5d63743244a034cd12a2fbdc57b8ba5
ua1891 <ua1891@gmail.com>
2026-05-06 14:42:23 +0500

    Finding max value

---
+++ Binary search Tree/Task5.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+struct BST{
+    int data;
+    BST* left;
+    BST* right;
+
+    BST(int value){
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+BST* insert(BST* root, int value){
+    if(root == NULL){
+        return new BST(value);
+    }
+    if(value < root->data){
+        root->left = insert(root->left, value);
+    }
+    else if(value > root->data){
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+int CountMax(BST* root){
+    if(root == NULL){
+        return 0;
+    }
+    return max(root->data, max(CountMax(root->left), CountMax(root->right)));
+}
+int main(){
+    BST* root = NULL;
+
+    root = insert(root, 5);
+    root = insert(root, 1);
+    root = insert(root, 3);
+    root = insert(root, 4);
+    root = insert(root, 2);
+    root = insert(root, 7);
+    int max = CountMax(root);
+    cout<<"Inorder Traversal: ";
+    PrintValue(root);
+    cout<<endl;
+    cout<<"Maximum Value: "<<max<<endl;
+    return 0;
+}

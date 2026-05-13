24b41a79255e8f28943c6cdb9d0a7207c7ee00d9
ua1891 <ua1891@gmail.com>
2026-05-13 14:50:41 +0500

    Inorder successor Inorder predecessor and comapre

---
+++ BST Opreation/Task6.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+using namespace std;
+#include <iostream>
+using namespace std;
+
+struct BST
+{
+    int data;
+    BST *left;
+    BST *right;
+
+    BST(int value)
+    {
+        data = value;
+        left = NULL;
+        right = NULL;
+    }
+};
+BST *insert(BST *root, int value)
+{
+    if (root == NULL)
+    {
+        return new BST(value);
+    }
+    if (value < root->data)
+    {
+        root->left = insert(root->left, value);
+    }
+    else if (value > root->data)
+    {
+        root->right = insert(root->right, value);
+    }
+    return root;
+}
+void Inorder(BST* root){
+    if(root==NULL){
+        return;
+    }
+    Inorder(root->left);
+    cout<<root->data<<" ";
+    Inorder(root->right);
+}
+BST* inorderrSuccessor(BST* root){
+    BST* current=root;
+    while(current!=NULL && current->left!=NULL){
+        current=current->left;
+    }
+    return current;
+}
+BST* IndorderPredecessor(BST* root){
+    BST* current=root;
+    while(current!=NULL && current->right!=NULL){
+        current=current->right;
+    }
+    return current;
+}
+BST *delNode(BST *root, int key)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->data > key)
+    {
+        root->left = delNode(root->left, key);
+    }
+    else if (root->data < key)
+    {
+        root->right = delNode(root->right, key);
+    }
+    else
+    {
+        if (root->left == NULL)
+        {
+            return root->right;
+        }
+        if (root->right == NULL)
+        {
+            return root->left;
+        }
+        BST* Succ=inorderrSuccessor(root->right);
+        root->data=Succ->data;
+         root->right=delNode(root->right,Succ->data);
+
+    }
+    return root;
+}
+BST *delNodeByPredecessor(BST *root, int key)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->data > key)
+    {
+        root->left = delNode(root->left, key);
+    }
+    else if (root->data < key)
+    {
+        root->right = delNode(root->right, key);
+    }
+    else
+    {
+        if (root->left == NULL)
+        {
+            return root->right;
+        }
+        if (root->right == NULL)
+        {
+            return root->left;
+        }
+        BST* Succ=IndorderPredecessor(root->left);
+        root->data=Succ->data;
+         root->left=delNode(root->left,Succ->data);
+
+    }
+    return root;
+}
+
+
+int main()
+{
+    BST *root = NULL;
+
+    root = insert(root, 5);
+    root = insert(root, 1);
+    root = insert(root, 3);
+    root = insert(root, 4);
+    root = insert(root, 2);
+    root = insert(root, 7);
+    cout<<"Inorder Traversal before Deletion: \n";
+    Inorder(root);
+    cout<<"Enter the Node which you want to delete: \n";
+    int DelNode=0;
+    cin>>DelNode;
+    root=delNode(root,DelNode);
+    cout<<"Inorder Traversal after Deletion:using Ancesstor \n";
+    Inorder(root);
+        cout<<"Enter the Node which you want to delete: \n";
+    cin>>DelNode;
+    root=delNodeByPredecessor(root,DelNode);
+    cout<<"Inorder Traversal after Deletion:using Predecessor \n";
+    Inorder(root);
+    return 0;
+}

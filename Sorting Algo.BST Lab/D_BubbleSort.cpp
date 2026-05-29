db14ec0e4ecb8ae050a8e9dd9f84eed7f612e346
ua1891 <ua1891@gmail.com>
2026-05-29 19:53:50 +0500

    OptimizedBubbleSort using Bool Variable to reduce unnessaccary Check

--- Sorting Algo.BST Lab/D_BubbleSort.cpp
+++ Sorting Algo.BST Lab/D_BubbleSort.cpp
@@ -15,6 +15,21 @@ void Bubblesort(int arr[], int n) {
         }
     }
 }
+void OptimizedBubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
 int main(){
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -23,5 +38,12 @@ int main(){
     Bubblesort(arr, n);
     cout << "Sorted array: ";
     PrintArray(arr, n);
+        int arr2[] = {64, 34, 25, 12, 22, 11, 90};
+    cout << "Original array: ";
+    PrintArray(arr2, n);
+    OptimizedBubbleSort(arr2, n);
+    cout << " After Optimization Sorted array: ";
+    PrintArray(arr2, n);
+
     return 0;
 }
